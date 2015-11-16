559b23727a6ed4fd5f98c8db0e2add53d69ffd41
MehdiZ93 <mehdi.zerfaoui@gmail.com>
2015-11-16 16:56:22 +0100

    Commit 3
    #3

--- application/accueil.php
+++ application/accueil.php
@@ -11,12 +11,17 @@
 	        die('Erreur : ' . $e->getMessage());
 	}
 	$html .= "
-		<div id = \"nav\">
+		<div id=\"nav\">
+			<div id=\"nav2\">
+				Application Facture
+			</div>
+			<div id=\"nav3\">
 			<ul>
-				 <li><a href=\"#\">Item 1</a></li>
+				 <li><a href=\"accueil.php\">Acceuil</a></li>
 				 <li><a href=\"#\">Item 2</a></li>
 				 <li><a href=\"#\">Item plus long</a></li>
 			</ul>
+			</div>
 		</div>
 	</html>";
 	echo $html;
